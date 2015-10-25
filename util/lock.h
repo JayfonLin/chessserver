83f6433a76e1909cd46475b3236b5bc5807a0fae
JayfonLin <linjiafang33@163.com>
2015-10-24 20:49:14 -0400

    util files

---
+++ util/lock.h
@@ -0,0 +1,21 @@
+
+#ifndef LOCK_H
+#define LOCK_H
+
+/*
+Created on 2015-10-16
+
+@author: jeff
+*/
+
+class CLock
+{
+public:
+    CLock();
+    ~CLock();
+
+private:
+
+};
+
+#endif
---
+++ util/log.h
@@ -0,0 +1,18 @@
+
+#ifndef LOG_H
+#define LOG_H
+
+/*
+Created on 2015-10-16
+
+@author: jeff
+*/
+
+class CLog
+{
+
+};
+
+#endif
+
+
